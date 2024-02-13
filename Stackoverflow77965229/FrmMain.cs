70d65abd17e1397a9a74252574b8ab92163f93f3
Alex Virlan <dj.3agl3@gmail.com>
2024-02-13 17:50:55 +0200

    Updating the forms

---
+++ Stackoverflow77965229/FrmMain.cs
@@ -0,0 +1,15 @@
+namespace Stackoverflow77965229
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
